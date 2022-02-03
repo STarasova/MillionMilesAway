cbc7ff2e7f104a509e56bb915f5e0283513e10f8
sergeytar <sergey.al.tarasov@gmail.com>
2022-02-03 18:57:52 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 126;
+        int oneBonusMilePrice = 20;
+        int bonusMiles = ticketPrice / oneBonusMilePrice;
+        System.out.println("Вам начислено " + bonusMiles + " бонусных миль.");
+    }
+}
